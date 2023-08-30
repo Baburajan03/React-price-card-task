e9d998bd0a408912cc4540912298c6b0b85fee18
Baburajan03 <baburajan.r03@gmail.com>
2023-08-30 18:11:47 +0530

    react price card task deploy

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import { Button } from 'react-bootstrap';
 import App from './App';
 
 library.add(fas);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
